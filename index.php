e8400a8d7d3fddd1aaa38f5ac5a22faab0f03f50
Joel Yek <joevo2@gmail.com>
2014-07-02 13:37:09 +0800

    added nav, main and header

--- index.php
+++ index.php
@@ -5,21 +5,28 @@
     <link rel='stylesheet' type="text/css" href="style.css">
   </head>
   <body>
-    <h1>HELP CAT Suggestion and Issue</h1>
-    <a href='signup.php'>Sign Up</a>
-    <a href='signin.php'>Sign In</a>
-    <a href='admin.php'>Admin</a>
-    <br>
-    <h2>Issue Submition</h2>
-    <form>
-      <input type='text' placeholder='Issue'><br>
-      <input type='text' placeholder='Location'><br>
-      To insert multiple tag example "lab, toilet, room 517, library"<br>
-      <input type='text' placeholder='tag'><br>
-      <input type='submit' value="Submit"><br>
-    </form>
-    <?php
-      queryDisplay(queryAll($con, "complaint"));
-    ?>
+    <header>
+      <h1>HELP CAT Suggestion and Issue</h1>
+      <nav>
+        <a href='signup.php'>Sign Up</a>
+        <a href='signin.php'>Sign In</a>
+        <a href='admin.php'>Admin</a>
+      </nav>
+    </header>
+    <main>
+
+      <br>
+      <h2>Issue Submition</h2>
+      <form>
+        <input type='text' placeholder='Issue'><br>
+        <input type='text' placeholder='Location'><br>
+        To insert multiple tag example "lab, toilet, room 517, library"<br>
+        <input type='text' placeholder='tag'><br>
+        <input type='submit' value="Submit"><br>
+      </form>
+      <?php
+        queryDisplay(queryAll($con, "complaint"));
+      ?>
+    </main>
   </body>
 </html>
