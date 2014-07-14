9c322ed26f8bb7f0d09ab303341d3bdf94b55b8a
Joel Yek <joevo2@gmail.com>
2014-07-14 11:19:27 +0800

    sign up form completed without email and password length verification

--- signup.php
+++ signup.php
@@ -10,14 +10,31 @@
     <h1>Sign Up</h1>
     <?php
       include "function.php";
-      if(isset()) {
-        queryInsert();
+      $email = $password = $cpassword = "";
+
+      if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['cpassword'])) {
+        // escape variables for security
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $password = mysqli_real_escape_string($con, $_POST['password']);
+        $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
+
+        if ($password == $cpassword) {
+          $sql="INSERT INTO user (email, password)
+          VALUES ('$email', '$password')";
+
+          if (!mysqli_query($con,$sql)) {
+            die('Error: ' . mysqli_error($con));
+          }
+        } else {
+          $_SESSION['error'] = "Password does not match";
+        }
       }
     ?>
+    <?php if (!empty($_SESSION['error'])) {echo $_SESSION['error'];}?>
     <form method='post'>
-      <input type='text' placeholder='Email'><br>
-      <input type='text' placeholder='Password'><br>
-      <input type='text' placeholder='Confirm Password'><br>
+      <input type='text' name='email' placeholder='Email'><br>
+      <input type='password' name='password' placeholder='Password'><br>
+      <input type='password' name='cpassword' placeholder='Confirm Password'><br>
       <input type='submit' value="Sign Up"><br>
     </form>
   </body>
