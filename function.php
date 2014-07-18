54b795298cfca9b4fa8e4950842c6af47f03fcde
Joel Yek <joevo2@gmail.com>
2014-07-18 08:17:53 +0800

    moved logic to function.php

--- function.php
+++ function.php
@@ -63,4 +63,36 @@
       }
     }
 
+    //Sign up logic
+    $email = $password = $cpassword = "";
+
+    if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['cpassword'])) {
+      // escape variables for security
+      $email = mysqli_real_escape_string($con, $_POST['email']);
+      $password = mysqli_real_escape_string($con, $_POST['password']);
+      $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
+
+      //email validation
+      if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      //password length validation
+        if (strlen($password) >= 8) {
+          if ($password == $cpassword) {
+            $sql="INSERT INTO user (email, password)
+            VALUES ('$email', '$password')";
+            if (mysqli_query($con,$sql)) {
+              header('Location: success.php');
+            } else {
+              die('Error: ' . mysqli_error($con));
+            }
+          } else {
+            $_SESSION['error'] = "Password does not match";
+          }
+        } else {
+          $_SESSION['error'] = "Password must be more than 8 character";
+        }
+      } else {
+        $_SESSION['error'] = "Wrong email format";
+      }
+    }
+
 ?>
--- signup.php
+++ signup.php
@@ -1,9 +1,9 @@
 <?php
+  session_start();
   //if logged in then redirect to index.php
   if (isset($_SESSION['signin'])) {
     header('Location: index.php');
   }
-  session_start();
 ?>
 <html>
   <head>
@@ -15,39 +15,11 @@
     <a href='index.php'><h1>HELP CAT Suggestion and Issue</h1></a>
     <h1>Sign Up</h1>
     <?php
+      //Sign up logic
       include "function.php";
-      $email = $password = $cpassword = "";
 
-      if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['cpassword'])) {
-        // escape variables for security
-        $email = mysqli_real_escape_string($con, $_POST['email']);
-        $password = mysqli_real_escape_string($con, $_POST['password']);
-        $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
-
-        //email validation
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        //password length validation
-          if (strlen($password) >= 8) {
-            if ($password == $cpassword) {
-              $sql="INSERT INTO user (email, password)
-              VALUES ('$email', '$password')";
-              if (mysqli_query($con,$sql)) {
-                header('Location: success.php');
-              } else {
-                die('Error: ' . mysqli_error($con));
-              }
-            } else {
-              $_SESSION['error'] = "Password does not match";
-            }
-          } else {
-            $_SESSION['error'] = "Password must be more than 8 character";
-          }
-        } else {
-          $_SESSION['error'] = "Wrong email format";
-        }
-      }
+      if (!empty($_SESSION['error'])) {echo $_SESSION['error'];}
     ?>
-    <?php if (!empty($_SESSION['error'])) {echo $_SESSION['error'];}?>
     <form method='post'>
       <input type='text' name='email' placeholder='Email'><br>
       <input type='password' name='password' placeholder='Password'><br>
