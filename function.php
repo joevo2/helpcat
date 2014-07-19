1ab09c6cb22c77cad379f609e746721de6628b29
Joel Yek <joevo2@gmail.com>
2014-07-19 10:51:17 +0800

    implemented sign in

--- function.php
+++ function.php
@@ -95,4 +95,27 @@
       }
     }
 
+    //Sign in logic
+    include "function.php";
+
+    $email = $password = "";
+    $_SESSION['error'] = "";
+
+    //Check form for non empty field and assigned to variable
+    if (!empty($_POST['email']) && !empty($_POST['password'])) {
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+
+      //Read from database and check password
+      $result = mysqli_query($con,"SELECT * FROM user");
+      while($row = mysqli_fetch_array($result)) {
+        if ($email == $row['email'] && $password == $row['password']) {
+          $_SESSION['signin'] = true;
+          header('Location: index.php');
+        } else {
+          $_SESSION['error'] = "Wrong username or password";
+        }
+      }
+    }
+
 ?>
