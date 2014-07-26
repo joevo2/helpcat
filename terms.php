944b0587d41d047283ddaed354e09c705c0f53ca
Joel Yek <joevo2@gmail.com>
2014-07-26 11:47:27 +0800

    added terms

---
+++ terms.php
@@ -0,0 +1,41 @@
+<?php session_start(); include "logic/indexlogic.php";?>
+<html>
+  <head>
+    <title>HELP CAT Suggestion and Issue</title>
+    <link rel='stylesheet' type="text/css" href="style.css">
+    <meta name="viewport" content="width=device-width">
+    <style>
+     h2 {
+       padding: 2% 5%;
+       font-size: 40px;
+       margin: 0 auto;
+     }
+     </style>
+  </head>
+
+    <body>
+        <div class="logonav">
+            <div id="logo">HELP CAT</div>
+        </div>
+        <header>
+            <h1 id="title">HELP CAT Suggestion and Issue</h1>
+            <?php include "nav.php"; ?>
+        </header>
+    <main>
+      <h2>Terms and Condition</h2>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae ornare felis. Curabitur cursus imperdiet lorem, sed facilisis tellus. Nunc et elit ac neque semper porta in quis sem. Vivamus at tincidunt sem, mollis scelerisque ante. In ornare egestas nisi nec aliquet. Phasellus in vulputate velit. Donec id accumsan urna. Nulla dolor enim, egestas ac turpis eget, bibendum dignissim leo. Maecenas lobortis, elit vel vulputate porta, ante nisi viverra diam, facilisis accumsan diam massa ut purus.
+
+Curabitur volutpat scelerisque mi, a consequat dui pellentesque in. Maecenas vulputate enim quis lacus pretium, quis volutpat nibh tincidunt. Pellentesque scelerisque mauris purus, in rhoncus ante pellentesque eget. Suspendisse felis augue, hendrerit in dui sed, rutrum viverra velit. In eget purus tempus, mattis mi ac, suscipit augue. Praesent dolor orci, aliquam quis lacus ut, dignissim ultricies mauris. Phasellus placerat, justo non mattis interdum, massa metus consectetur urna, et dapibus lacus metus tempus libero. Mauris viverra tempus orci in volutpat. Duis convallis elit eget mi euismod, ac placerat sem feugiat. Proin nunc nibh, fermentum id quam eget, aliquet sollicitudin urna. Vivamus tincidunt bibendum neque a volutpat. Mauris velit nunc, pretium et libero et, posuere aliquet risus. Etiam eleifend erat at odio vulputate condimentum.
+
+Suspendisse ac volutpat lacus. Mauris bibendum felis leo, eget blandit augue facilisis et. Aliquam cursus bibendum volutpat. Integer dui augue, elementum nec mauris in, mattis sagittis felis. Nulla lobortis hendrerit leo vitae fringilla. Donec blandit nibh non ligula accumsan vestibulum. Pellentesque adipiscing quam quis eros ullamcorper placerat. Aliquam vitae ultricies dolor, ac pellentesque elit. Ut convallis neque ac lorem bibendum hendrerit. Duis placerat placerat leo, sed accumsan sem commodo in. Sed euismod felis in orci semper facilisis. Praesent tincidunt tortor lobortis tellus congue, eget cursus tortor adipiscing. Quisque vel volutpat ipsum.
+
+Suspendisse potenti. Phasellus vestibulum elit sit amet tellus porta semper. Morbi nec turpis non nulla posuere congue vitae ac mauris. Aenean rhoncus ultrices felis, eleifend accumsan mauris hendrerit vel. Praesent aliquam luctus auctor. Donec volutpat a nisl vel cursus. Vivamus ac semper purus. Fusce a quam leo. Etiam blandit urna nisi, sed dapibus libero venenatis in. Sed turpis lorem, porttitor vel arcu sollicitudin, imperdiet mattis justo.
+
+Curabitur nibh magna, consequat vitae fermentum et, pellentesque eget dolor. Phasellus vulputate ligula nec nisi sagittis vulputate. Phasellus condimentum dolor non nulla auctor, congue euismod dolor vehicula. Donec mollis libero enim, eu dapibus magna interdum ac. Praesent sit amet elementum erat. Vestibulum ut volutpat ligula. Sed gravida bibendum ligula vel pellentesque. Praesent quis purus ut tortor pharetra bibendum eu sed justo. Fusce feugiat justo a ante mattis laoreet. Aliquam eu tortor ac leo ornare lacinia. Sed id aliquam libero. Proin in purus turpis. Praesent eu facilisis lectus. Vestibulum massa neque, molestie id posuere et, hendrerit vitae arcu.
+        </p>
+      </div>
+    </main>
+  </body>
+</html>
